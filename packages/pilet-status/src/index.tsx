95b03e94ad19282c79ef46ff38d22a7ff6e1a726
Florian Rappl <florian.rappl@smapiot.com>
2020-06-06 03:23:05 +0200

    Introduced `watch:all` command

--- packages/pilet-status/src/index.tsx
+++ packages/pilet-status/src/index.tsx
@@ -2,8 +2,5 @@ import * as React from 'react';
 import { PiletApi } from 'portal';
 
 export function setup(app: PiletApi) {
-  app.registerTile(() => <div>Welcome to Piral!</div>, {
-    initialColumns: 2,
-    initialRows: 1,
-  });
+  app.registerTile(() => <div>Welcome to Piral!</div>);
 }
